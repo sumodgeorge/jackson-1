8418ee61642927b591927bb212a47c209d549e23
Deepak Prabhakara <deepak@boxyhq.com>
2022-03-26 00:31:13 +0000

    ts -> js for migration scripts

--- npm/ormconfig.js
+++ npm/ormconfig.js
@@ -5,8 +5,8 @@ module.exports = {
   url: process.env.DB_URL || 'postgresql://postgres:postgres@localhost:5432/postgres',
   synchronize: false,
   logging: true,
-  entities: ['src/db/sql/entity/**/*.ts'],
-  migrations: [`migration/${type}/**/*.ts`],
+  entities: ['src/db/sql/entity/**/*.js'],
+  migrations: [`migration/${type}/**/*.js`],
   cli: {
     entitiesDir: 'src/db/sql/entity',
     migrationsDir: `migration/${type}`,
